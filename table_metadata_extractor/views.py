cbc28091f08ea018c46255e9f4a0666b02623ec1
Vivian Ling <vivianling@Vivians-Air.lan>
2023-05-21 21:58:22 -0600

    Fetch num_rows, schema, and database

--- table_metadata_extractor/views.py
+++ table_metadata_extractor/views.py
@@ -1,11 +1,12 @@
 from django.http import JsonResponse
 from sqlalchemy import create_engine, inspect
+from sqlalchemy.sql import text
 
 def table_metadata_view(request):
     connection_string = request.GET.get('connection_string')
 
     if not connection_string:
-        return JsonResponse({'error': 'No connection string provided'})
+        return JsonResponse({'error': 'No connection string provided. Please provide an SQLAlchemy-supported connection string in `connection_string` query paramater'})
 
     try:
         engine = create_engine(connection_string)
@@ -13,24 +14,27 @@ def table_metadata_view(request):
         tables = inspector.get_table_names()
 
         table_metadata_list = []
-        for table_name in tables:
-            columns = inspector.get_columns(table_name)
-            num_rows = 0
-            schema = ''
-            database = ''
+        with engine.connect() as connection:
+            for table_name in tables:
+                columns = inspector.get_columns(table_name)
+                num_rows = connection.execute(text(f"SELECT COUNT(*) FROM {table_name}")).fetchone()[0]
+                database = engine.url.database
 
-            column_metadata_list = [
-                {"name": column['name'], "type": str(column['type'])} for column in columns
-            ]
+                schema_query = text("SELECT table_schema FROM information_schema.tables WHERE table_name = :table_name")
+                schema = connection.execute(schema_query, {"table_name": table_name}).fetchone()[0]
 
-            table_metadata = {
-                "columns": column_metadata_list,
-                "num_rows": num_rows,
-                "schema": schema,
-                "database": database
-            }
+                column_metadata_list = [
+                    {"name": column['name'], "type": str(column['type'])} for column in columns
+                ]
 
-            table_metadata_list.append(table_metadata)
+                table_metadata = {
+                    "columns": column_metadata_list,
+                    "num_rows": num_rows,
+                    "schema": schema,
+                    "database": database
+                }
+
+                table_metadata_list.append(table_metadata)
 
         return JsonResponse({'tables': table_metadata_list})
 
